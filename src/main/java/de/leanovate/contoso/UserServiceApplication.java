0d31533cdfc583a5ff64c1730b721a0213457455
Marco Reinwarth <marco.reinwarth@leanovate.de>
2017-03-17 13:30:17 +0100

    Connect user service to Kafka
     * switched back to maven
     * downgraded spring boot to align with old Kafka version

--- src/main/java/de/leanovate/contoso/UserServiceApplication.java
+++ src/main/java/de/leanovate/contoso/UserServiceApplication.java
@@ -2,11 +2,25 @@ package de.leanovate.contoso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.core.MessageSource;
+import org.springframework.messaging.support.GenericMessage;
 
 @SpringBootApplication
+@EnableBinding(Source.class)
 public class UserServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+    @Bean
+    @InboundChannelAdapter(value = Source.OUTPUT)
+    public MessageSource<String> timerMessageSource() {
+        return () -> new GenericMessage<>(String.format("UserService: %d", System.currentTimeMillis()));
+    }
+
 }
